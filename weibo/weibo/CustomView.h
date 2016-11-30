fea97a2a56eefeaf66f6398ea908c75751cf6b56
北京 <>
2016-11-30 16:59:46 +0800

    创建了自定义view

---
+++ weibo/weibo/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  weibo
+//
+//  Created by 北京启智 on 2016/11/30.
+//  Copyright © 2016年 北京启智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ weibo/weibo/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  weibo
+//
+//  Created by 北京启智 on 2016/11/30.
+//  Copyright © 2016年 北京启智. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
